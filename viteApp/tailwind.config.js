247da05d79c287c4dce8d214a978644d14f8244a
Eduardo Bravo <yahir.eduardo.bravo@gmail.com>
2023-10-17 21:41:13 -0500

    Feat: add packages configs and formatters

--- viteApp/tailwind.config.js
+++ viteApp/tailwind.config.js
@@ -1,9 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: ['./src/**/*.{html,js}', 'index.html'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['Montserrat', 'sans-serif']
+      }
+    }
   },
-  plugins: [],
+  plugins: []
 }
-
